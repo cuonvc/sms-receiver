84f644736cb3e17c5fccb6ef1c5420d5dc0174cb
cuonvc <cuong.fithau@gmail.com>
2024-05-20 22:08:01 +0700

    update/api integrate

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:readsms/readsms.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -37,7 +40,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   final _plugin = Readsms();
-  String sms = '';
+  String content = '';
   String sender = '';
   String time = '';
   String pendingText = 'Đang đợi ting ting từ người ấy...';
@@ -49,14 +52,29 @@ class _MyHomePageState extends State<MyHomePage> {
     getPermission().then((value) {
       if (value) {
         _plugin.read();
-        _plugin.smsStream.listen((event) {
+        _plugin.smsStream.listen((event) async {
           setState(() {
-            sms = 'Nội dung: ' + event.body;
+            content = 'Nội dung: ' + event.body;
             sender = 'Người gửi: ' + event.sender;
             time = 'Thời gian: ' + event.timeReceived.toString();
             pendingText = 'Có tin nhắn mới...';
             imagePath = 'image/3nRK.gif';
           });
+
+          //post message data to the HAU-Ecommerce server
+          final uri = Uri.parse("https://viper-chief-secondly.ngrok-free.app/api/order/wallet/deposit/submit");
+          Map<String, dynamic> request = {
+            'sender': event.sender,
+            'content': event.body,
+            'time': event.timeReceived.toString()
+          };
+
+          final response = await http.post(
+              uri,
+              headers: {"Content-Type": "application/json"},
+              body: json.encode(request)
+          );
+          print('Response sms - ' + response.statusCode.toString());
         });
       }
     });
@@ -109,7 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Text(sender, style: TextStyle(fontSize: 16),),
-            Text(sms, style: TextStyle(fontSize: 16),),
+            Text(content, style: TextStyle(fontSize: 16),),
             Text(time, style: TextStyle(fontSize: 16),)
           ],
         ),
